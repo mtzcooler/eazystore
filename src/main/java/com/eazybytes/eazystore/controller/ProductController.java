6237b5597926034d4ffb3e93d8c56ad608f85fb1
Matheus <mtzcooler@gmail.com>
2025-07-24 12:12:52 +0100

    Add service layer

--- src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazystore.controller;
 
 import com.eazybytes.eazystore.entity.Product;
 import com.eazybytes.eazystore.repository.ProductRepository;
+import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final IProductService iProductService;
 
     @GetMapping
     public List<Product> getProducts() {
-        List<Product> products = productRepository.findAll();
+        List<Product> products = iProductService.getProducts();
         return products;
     }
 }
---
+++ src/main/java/com/eazybytes/eazystore/service/IProductService.java
@@ -0,0 +1,10 @@
+package com.eazybytes.eazystore.service;
+
+import com.eazybytes.eazystore.entity.Product;
+
+import java.util.List;
+
+public interface IProductService {
+
+    List<Product> getProducts();
+}
