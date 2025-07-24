f1f281782eea65bcc0c6cb97288b3a5ec21462dc
Matheus <mtzcooler@gmail.com>
2025-07-24 16:02:48 +0100

    Implement DTO layer

--- src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,10 +1,8 @@
 package com.eazybytes.eazystore.controller;
 
-import com.eazybytes.eazystore.entity.Product;
-import com.eazybytes.eazystore.repository.ProductRepository;
+import com.eazybytes.eazystore.dto.ProductDto;
 import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +17,8 @@ public class ProductController {
     private final IProductService iProductService;
 
     @GetMapping
-    public List<Product> getProducts() {
-        List<Product> products = iProductService.getProducts();
+    public List<ProductDto> getProducts() {
+        List<ProductDto> products = iProductService.getProducts();
         return products;
     }
 }
