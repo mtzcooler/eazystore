0c46dae96197089a869f33f978ef54c97d12fe13
Matheus <mtzcooler@gmail.com>
2025-08-03 17:21:50 +0100

    Enable JPA Auditing

--- src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -2,9 +2,10 @@ package com.eazybytes.eazystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-// @ComponentScan(basePackages = {"com.eazybytes.eazystore.controller"})
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class EazystoreApplication {
 
 	public static void main(String[] args) {
